2ab347535d5cdffacb268edc3f50592ed83d9b2e
asakhar <a@sakhar.ru>
2020-10-31 01:11:07 +0300

    doxygen documentation added

--- ParamQueue.hpp
+++ ParamQueue.hpp
@@ -5,44 +5,104 @@
 #include <string>
 #include <vector>
 
+/**
+ * @brief Queue element
+ *
+ * @tparam __n String length
+ */
 template <const size_t __n>
 struct QueueElem
 {
   int num;
   char str[__n];
+  /**
+   * @brief Construct a new Queue Elem object
+   *
+   * @param _num Number data
+   * @param _str String data of length __n
+   */
   QueueElem(int const& _num = 0, char const _str[__n] = nullptr) : num(_num)
   {
     std::memset(reinterpret_cast<char*>(str), 0, sizeof(str));
     if (_str)
       std::memcpy(reinterpret_cast<char*>(str), reinterpret_cast<const char*>(_str), std::min(std::strlen(_str), sizeof(str)));
   }
+  /**
+   * @brief Input from stream operator
+   *
+   * @param stream e.g. std::cin
+   * @param elem element to be filled from stream
+   * @return std::istream&
+   */
   friend std::istream& operator>>(std::istream& stream, QueueElem<__n>& elem)
   {
     return (stream >> elem.num).get(*elem.str).get(elem.str, __n);
   }
+  /**
+   * @brief Output to stream operator
+   *
+   * @param stream e.g. std::cout
+   * @param elem element to be translated to stream
+   * @return std::ostream&
+   */
   friend std::ostream& operator<<(std::ostream& stream, QueueElem<__n>& elem)
   {
     return stream << "(" << elem.num << ", " << elem.str << ")";
   }
 };
 
+/**
+ * @brief Queue filling state enum
+ *
+ */
 enum QueueState
-{
+{ /**
+   * @brief Empty
+   *
+   */
   empt,
+  /**
+   * @brief Partially full
+   *
+   */
   part,
+  /**
+   * @brief Full
+   *
+   */
   full
 };
+/**
+ * @brief Parameter Queue
+ *
+ * @tparam Item Queue element type
+ * @tparam __n Size of vector
+ */
 template <typename Item, const size_t __n>
 struct ParamQueue
 {
+private:
   size_t front, back;
   Item queue[__n];
 
+public:
+  /**
+   * @brief Construct a new ParamQueue<Item, __n> object from std::vector of elements
+   *
+   * @param _q elements
+   */
   ParamQueue<Item, __n>(std::vector<Item> _q = {}) : front(0), back(std::min(__n, _q.size()))
   {
     if (!_q.empty())
       std::memcpy(queue, _q.data(), std::min(sizeof(queue), sizeof(Item) * _q.size()));
   }
+  /**
+   * @brief Input one element from stream operator
+   *
+   * @param stream e.g. std::cin
+   * @param q Queue
+   * @return std::istream&
+   */
   friend std::istream& operator>>(std::istream& stream, ParamQueue<Item, __n>& q)
   {
     if ((q.back >= __n) != (q.front >= __n) && q.back % __n == q.front % __n)
@@ -51,6 +111,13 @@ struct ParamQueue
     q.back %= 2 * __n;
     return stream;
   }
+  /**
+   * @brief Output to stream operator
+   *
+   * @param stream e.g. std::cout
+   * @param q Queue to be printed to stream
+   * @return std::ostream&
+   */
   friend std::ostream& operator<<(std::ostream& stream, ParamQueue<Item, __n>& q)
   {
     for (size_t i = q.front; i != q.back; (++i) %= 2 * __n)
@@ -59,6 +126,12 @@ struct ParamQueue
     }
     return stream;
   }
+  /**
+   * @brief Take first element from queue
+   *
+   * @param elem Reference to an object to fill
+   * @return ParamQueue<Item, __n>&
+   */
   ParamQueue<Item, __n>& operator()(Item& elem)
   {
     if ((back >= __n) == (front >= __n) && back % __n == front % __n)
@@ -67,6 +140,12 @@ struct ParamQueue
     front %= 2 * __n;
     return *this;
   }
+  /**
+   * @brief Add to queue operator
+   *
+   * @param elem Element to be added
+   * @return Item&
+   */
   Item& operator+=(Item const& elem)
   {
     if ((back >= __n) != (front >= __n) && back % __n == front % __n)
@@ -75,7 +154,12 @@ struct ParamQueue
     back %= 2 * __n;
     return queue[(back - 1) % __n];
   }
-  QueueState check_state()
+  /**
+   * @brief Empty, partially full & full state checking
+   *
+   * @return QueueState
+   */
+  QueueState check_state() const
   {
     if (back % __n == front % __n)
     {
@@ -88,6 +172,13 @@ struct ParamQueue
       return QueueState::part;
   }
 };
+/**
+ * @brief State output format operator
+ *
+ * @param stream e.g. std::cout
+ * @param state
+ * @return std::ostream&
+ */
 std::ostream& operator<<(std::ostream& stream, QueueState state)
 {
   return stream << (state != QueueState::empt ? (state == QueueState::part ? "Partially full" : "Full") : "Empty");
--- ParamQueueDynamic.hpp
+++ ParamQueueDynamic.hpp
@@ -5,6 +5,10 @@
 #include <string>
 #include <vector>
 
+/**
+ * @brief Queue element
+ *
+ */
 struct QueueElem
 {
   int num;
@@ -13,21 +17,49 @@ private:
   std::unique_ptr<char[]> str;
 
 public:
+  /**
+   * @brief Construct a new Queue Elem object
+   *
+   * @param _num Number data
+   * @param _str String data
+   */
   QueueElem(int const& _num = 0, std::string const& _str = "") : num(_num), str(new char[_str.size() + 1])
   {
     std::memcpy(str.get(), _str.c_str(), _str.size() + 1);
   }
+  /**
+   * @brief Copy-construct a new Queue Elem object
+   *
+   * @param other Const reference of an object to be copied from
+   */
   QueueElem(QueueElem const& other) : num{other.num}, str(new char[std::strlen(other.str.get()) + 1])
   {
     std::memcpy(str.get(), other.str.get(), std::strlen(other.str.get()) + 1);
   }
+  /**
+   * @brief Move-construct a new Queue Elem object
+   *
+   * @param other rvalue reference of an object to be moved from
+   */
   QueueElem(QueueElem&& other) : num{other.num}, str(std::move(other.str)){};
+  /**
+   * @brief Move-assignment operator
+   *
+   * @param other rvalue reference of an object to be moved from
+   * @return QueueElem&
+   */
   QueueElem& operator=(QueueElem&& other)
   {
     num = other.num;
     str = std::move(other.str);
     return *this;
   }
+  /**
+   * @brief Copy-assignment operator
+   *
+   * @param other Const reference of an object to be copied from
+   * @return QueueElem&
+   */
   QueueElem& operator=(QueueElem const& other)
   {
     num = other.num;
@@ -35,6 +67,13 @@ public:
     std::memcpy(str.get(), other.str.get(), std::strlen(other.str.get()) + 1);
     return *this;
   }
+  /**
+   * @brief Input from stream
+   *
+   * @param stream
+   * @param elem
+   * @return std::istream&
+   */
   friend std::istream& operator>>(std::istream& stream, QueueElem& elem)
   {
     stream >> elem.num;
@@ -47,28 +86,67 @@ public:
 
     return stream;
   }
+  /**
+   * @brief Output to stream
+   *
+   * @param stream
+   * @param elem
+   * @return std::ostream&
+   */
   friend std::ostream& operator<<(std::ostream& stream, QueueElem& elem)
   {
     return stream << "(" << elem.num << ", " << elem.str.get() << ")";
   }
+  /**
+   * @brief Set the string
+   *
+   * @param value
+   * @return char const*
+   */
   char const* set_str(char const* value = "")
   {
     str.reset(new char[std::strlen(value) + 1]);
     std::memcpy(str.get(), value, std::strlen(value) + 1);
     return str.get();
   }
+  /**
+   * @brief Get the string object
+   *
+   * @return char const*
+   */
   char const* get_str() const { return str.get(); }
-  // int const& set_num(int const& value) { // not nessesserily to encapsulate
+  // int const& set_num(int const& value) { // not nessesserily to encapsulate number
   //   return num
   // }
 };
 
+/**
+ * @enum
+ * @brief Queue filling state enum
+ *
+ */
 enum QueueState
 {
-  empt,
-  part,
-  full
+  empt /**
+        * @brief Queue is empty
+        *
+        */
+  ,
+  part /**
+        * @brief Queue is partially full
+        *
+        */
+  ,
+  full /**
+        * @brief Queue is full
+        *
+        */
 };
+/**
+ * @brief Queue class
+ *
+ * @tparam Item Queue element type
+ */
 template <typename Item>
 struct ParamQueue
 {
@@ -79,13 +157,24 @@ private:
   size_t _vector_size;
 
 public:
-  ParamQueue<Item>(std::vector<Item> const& _q = {}) : _vector_size(_q.capacity()), back(_q.size()), front(0)
+  /**
+   * @brief Construct a new Param Queue object from std::vector of elements
+   *
+   * @param _q vector of elements
+   */
+  ParamQueue(std::vector<Item> const& _q = {}) : _vector_size(_q.capacity()), back(_q.size()), front(0)
   {
     head.reset(new Item[_q.capacity()]);
     for (size_t i = 0; i < _q.size(); i++)
       head[i] = _q[i];
   }
-  ParamQueue<Item>(ParamQueue<Item> const& other, size_t multiplier = 1) : back(0), front(0), _vector_size(other._vector_size)
+  /**
+   * @brief Copy-construct a new Param Queue object with possible size extencion
+   *
+   * @param other Object to copy from
+   * @param multiplier Size multiplication
+   */
+  ParamQueue(ParamQueue<Item> const& other, size_t multiplier = 1) : back(0), front(0), _vector_size(other._vector_size)
   {
     head.reset(new Item[other._vector_size * multiplier]);
     for (size_t i = other.front; i != other.back; (++i) %= 2 * _vector_size, back++)
@@ -94,11 +183,27 @@ public:
     }
     _vector_size *= multiplier;
   }
-  ParamQueue<Item>(ParamQueue<Item>&& other)
+  /**
+   * @brief Move-construct a new Param Queue object
+   *
+   * @param other rvalue reference to move from
+   */
+  ParamQueue(ParamQueue<Item>&& other)
       : _vector_size(other._vector_size), back(other.back), front(other.front), head(other.head.release())
   {
   }
+  /**
+   * @brief Increase vector size by mul times
+   *
+   * @param mul
+   */
   inline void resize(size_t mul) { *this = std::move(ParamQueue<Item>(*this, mul)); }
+  /**
+   * @brief Copy-assignment operator
+   *
+   * @param other Object to copy from
+   * @return ParamQueue<Item>&
+   */
   ParamQueue<Item>& operator=(ParamQueue<Item> const& other)
   {
     back         = 0;
@@ -111,6 +216,12 @@ public:
     }
     return *this;
   }
+  /**
+   * @brief Move-assignment operator
+   *
+   * @param other Object to move from
+   * @return ParamQueue<Item>&
+   */
   ParamQueue<Item>& operator=(ParamQueue<Item>&& other)
   {
     front        = other.front;
@@ -119,6 +230,13 @@ public:
     head.reset(other.head.release());
     return *this;
   }
+  /**
+   * @brief Input from stream operator
+   *
+   * @param stream e.g. std::cin
+   * @param q Object to fill
+   * @return std::istream&
+   */
   friend std::istream& operator>>(std::istream& stream, ParamQueue<Item>& q)
   {
     Item new_item;
@@ -126,12 +244,25 @@ public:
     q += new_item;
     return stream;
   }
+  /**
+   * @brief Output to stream operator
+   *
+   * @param stream e.g. std::cout
+   * @param q Queue to print to stream
+   * @return std::ostream&
+   */
   friend std::ostream& operator<<(std::ostream& stream, ParamQueue<Item>& q)
   {
     for (size_t i = q.front; i != q.back; (++i) %= 2 * q.capacity())
       stream << q.head[i % q.capacity()];
     return stream;
   }
+  /**
+   * @brief Take the first element from queue
+   *
+   * @param elem Object to fill reference
+   * @return ParamQueue<Item>&
+   */
   ParamQueue<Item>& operator()(Item& elem)
   {
     if ((back >= _vector_size) == (front >= _vector_size) && back % _vector_size == front % _vector_size)
@@ -140,6 +271,12 @@ public:
     front %= 2 * _vector_size;
     return *this;
   }
+  /**
+   * @brief Add element to queue
+   *
+   * @param elem
+   * @return Item&
+   */
   Item& operator+=(Item const& elem)
   {
     if ((back >= _vector_size) != (front >= _vector_size) && back % _vector_size == front % _vector_size)
@@ -149,7 +286,12 @@ public:
     back %= 2 * _vector_size;
     return head[(back - 1) % _vector_size];
   }
-  QueueState check_state()
+  /**
+   * @brief State cheking
+   *
+   * @return QueueState
+   */
+  QueueState check_state() const
   {
     if (back % _vector_size == front % _vector_size)
     {
@@ -161,7 +303,17 @@ public:
     else
       return QueueState::part;
   }
+  /**
+   * @brief Get vector capacity
+   *
+   * @return size_t
+   */
   inline size_t capacity() const { return _vector_size; }
+  /**
+   * @brief Get queue size
+   *
+   * @return size_t
+   */
   inline size_t size() const
   {
     if (back < front)
@@ -169,6 +321,13 @@ public:
     return back - front;
   }
 };
+/**
+ * @brief Queue state stream print operator
+ *
+ * @param stream e.g. std::cout
+ * @param state
+ * @return std::ostream&
+ */
 std::ostream& operator<<(std::ostream& stream, QueueState state)
 {
   return stream << (state != QueueState::empt ? "Not empty" : "Empty");
